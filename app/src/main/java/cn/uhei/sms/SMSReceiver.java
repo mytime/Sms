c30c40044f3fc5e93163c89633c01a725a707ab8
Administrator <cmba@qq.com>
2015-12-30 18:01:06 +0800

    拦截短信
    拦截短信

---
+++ app/src/main/java/cn/uhei/sms/SMSReceiver.java
@@ -0,0 +1,36 @@
+package cn.uhei.sms;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+/**
+ * Created by Administrator on 2015/12/30.
+ */
+public class SMSReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+//        //获取intent传过来的内容
+//        Bundle bundle = intent.getExtras();
+//        //提取短信
+//        Object[] pdus = (Object[]) bundle.get("pdus");
+//        // 构建短信对象数组；
+//        SmsMessage[] messages = new SmsMessage[pdus.length];
+//        for (int i = 0; i < messages.length; i++) {
+//            // 获取单条短信内容，以pdu格式存,并生成短信对象；
+//            messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+//        }
+//        //获取发送方号码
+//        String address = messages[0].getOriginatingAddress();
+//        String fullMessage = "";
+//        for (SmsMessage message : messages) {
+//            //获取短信内容
+//            fullMessage += message.getMessageBody();
+//        }
+//        System.out.println(">>>>>>>>>>>>>" + address + fullMessage);
+        System.out.println("日，有消息了。");
+
+    }
+}
